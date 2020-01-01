255735c1a7793fb1c60cd740e7ac5481a7d14356
Doss <ly47ly47@outlook.com>
2020-01-01 17:57:00 +0800

    完成了切换内容效

---
+++ js/doss.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', () => {
+    const article = document.querySelectorAll('article');
+    const sub_nav = document.querySelectorAll('.sub_nav a');
+    const nav_item = document.querySelectorAll('.join .nav_item li');
+    console.log(nav_item);
+    for(let i = 0; i < article.length; i++) {
+        // 利用闭包 立即执行函数
+        ( i => {
+            sub_nav[i].addEventListener('click', () => {
+                for(let i = 0; i < article.length; i++) {
+                    sub_nav[i].className = '';
+                    article[i].className = '';
+                }
+                sub_nav[i].className = 'current';
+                article[i].className = 'current';
+            });
+            nav_item[i].addEventListener('click', () => {
+                sub_nav[i].click();
+            });
+        })(i);
+    }
+});
